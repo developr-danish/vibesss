876525a46ec3eae2f74b23850d86f7e2c246cec9
danish4994 <danish0ahmad@gmail.com>
2024-04-03 18:11:31 +0530

    "update"

--- Script/main.js
+++ Script/main.js
@@ -1,20 +1,3 @@
-// on scroll effect
-var sticky = document.querySelector('.navbar');
-window.onscroll = function(){
-  if(window.scrollY > 0) {
-    sticky.style.height = '100px';
-    sticky.style.backgroundColor = "#EAE8E2";
-    sticky.style.transition = "0.3s";
-  }else {
-    sticky.style.height = '100px';
-    sticky.style.backgroundColor = "transparent";
-    logo.style.height = '180px';
-    logo.style.width = '180px';
-  }
-}
-// on scroll effect
-
-
 // top product carousel
 $(function() {
   
@@ -44,7 +27,7 @@ $(function() {
                 margin: 20,
                 stagePadding: 0,
                 nav: true,
-                items: 3
+                items: 4
               }
             }
           });            
